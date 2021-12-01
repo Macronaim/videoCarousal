d797c9333eee3775983dea9839a4cc04715414e4
Aere Naim <naimdeveloper@gmail.com>
2021-12-01 12:12:19 +0100

    carousal without videos

---
+++ videos.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.videoPop').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
